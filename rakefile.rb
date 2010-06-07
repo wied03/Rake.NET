619f659234c49b91dd063f58208406a2213930a7
wied03 <brady@wied.us>
2010-06-06 19:08:52 -0500

    Removed old YAML config stuff from project Setup rakefile with newly acquired knowledge

--- rakefile.rb
+++ rakefile.rb
@@ -1,18 +1,26 @@
 require 'rake'
 require 'rubygems'
 require 'rake/gempackagetask'
+require 'spec/rake/spectask'
+
+Spec::Rake::SpecTask.new(:spec) do |t|
+  t.spec_files = FileList['test/**/*_spec.rb']
+  t.spec_opts << '--format specdoc'
+  t.libs = FileList['test']
+end
 
 spec = Gem::Specification.new do |s|
   s.name = 'raketasks'
   s.version = '1.0.0'
   s.summary = "Rake tasks for building .NET projects"
-  s.description = "Rake tasks for building .NET projects"
+  s.description = s.summary
   s.files = FileList['lib/**/*.rb', 'test/**/*.rb']
   s.test_files = FileList['test/**/*.rb']
   s.has_rdoc = false
   s.author = "Brady Wied"
   s.email = "brady@wied.us"
   s.add_dependency('fastercsv', '>= 1.5.0')
+  s.add_dependency('yamlconfig')
   s.platform = 'mswin32'
 end
 
