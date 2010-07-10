d651ad346a0e66e6f33219f55e33a74ee70fea40
wied03 <brady@wied.us>
2010-07-10 15:29:56 -0500

    Use spec convention, introduced scope rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -4,37 +4,45 @@ require 'rake/gempackagetask'
 require 'spec/rake/spectask'
 require 'rake/rdoctask'
 require 'lib/version'
+require 'lib/tools'
 require 'ci/reporter/rake/rspec'
 
-Spec::Rake::SpecTask.new :spec do |t|
-  t.spec_files = FileList['test/**/*_spec.rb']
-  t.spec_opts << '--format specdoc'
-  t.libs = FileList['test']
-end
+with("spec") do |testdir|
+  Spec::Rake::SpecTask.new(:spec) do |t|
+    t.spec_files = FileList["#{testdir}/**/*_spec.rb"]
+    t.spec_opts << '--format specdoc'
+    t.libs = FileList[testdir]
+  end
 
-task :clean_install => [:repackage, :install]
+  task :clean_install => [:repackage, :install]
 
-task :install do
-  sh "gem install " +FileList['pkg/*.gem'].first()
-  sh "gem cleanup raketasks"
-end
+  with("rakedotnet") do |gemname|
+    task :install do
+      sh "gem install #{FileList['pkg/*.gem'].first()}"
+      sh "gem cleanup #{gemname}"
+    end
 
-spec = Gem::Specification.new do |s|
-  s.name = 'rakedotnet'
-  s.version = BW::Version.incrementandretrieve
-  s.summary = "Rake tasks for building .NET projects"
-  s.description = s.summary
-  s.files = FileList['lib/**/*.rb', 'test/**/*.rb']
-  s.test_files = FileList['test/**/*.rb']
-  s.has_rdoc = true
-  s.rdoc_options << '--inline-source' <<  '--line-numbers'
-  s.author = "Brady Wied"
-  s.email = "brady@wied.us"
-  s.add_dependency('fastercsv', '>= 1.5.0')
-  s.add_dependency('bwyamlconfig')
-  s.add_dependency('windows-pr')
-  s.platform = 'mswin32'
-end
+    with ("lib") do |src|
+      spec = Gem::Specification.new do |s|
+        s.name = gemname
+        s.files = FileList["#{src}/**/*.rb",
+                           "#{testdir}/**/*.rb"]
+        s.test_files = FileList["#{testdir}/**/*.rb"]
+        s.version = BW::Version.incrementandretrieve
+        s.summary = "Rake tasks for building .NET projects"
+        s.description = s.summary
+        s.has_rdoc = true
+        s.rdoc_options << '--inline-source' << '--line-numbers'
+        s.author = "Brady Wied"
+        s.email = "brady@wied.us"
+        s.add_dependency('fastercsv', '>= 1.5.0')
+        s.add_dependency('bwyamlconfig')
+        s.add_dependency('windows-pr')
+        s.platform = 'mswin32'
+      end
 
-Rake::GemPackageTask.new(spec) do |pkg|
+      Rake::GemPackageTask.new(spec) do |pkg|
+      end
+    end
+  end
 end
