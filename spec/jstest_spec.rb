f807186e353468b8314a646dfdbddef41af16989
wied03 <brady@wied.us>
2010-08-02 22:25:37 -0500

    Enhanced custom matcher to make tests more clean

--- spec/jstest_spec.rb
+++ spec/jstest_spec.rb
@@ -6,13 +6,24 @@ def jspath
   FileList["data/jstest/path/**/*.js"]
 end
 
-Spec::Matchers.define :same_js_config do |expected|
-    match do |actual|
-      expected['server'] == actual['server']
+Spec::Matchers.define :have_same_config_as do |e|
+    expected = YAML::load(File.read(e))
+    match do |a|
+      actual = YAML::load(File.read(a))
+      actual['server'].should == expected['server']
       expLoad = expected['load']
       actLoad = actual['load']
       expLoad.each { |file| actLoad.should include file }
       actLoad.each { |file| expLoad.should include file }
+    end
+
+    failure_message_for_should do |a|
+      actual = YAML::load(File.read(a))
+      expSvr = expected['server']
+      actSvr = actual['server']
+      expLoad = expected['load'].join("\n")
+      actLoad = actual['load'].join("\n")
+      "Expected server #{expSvr} but got #{actSvr}.\n\nExpected Files:\n#{expLoad}\n\nActual Files:\n#{actLoad}"
     end
 end
 
@@ -36,10 +47,7 @@ describe BW::JsTest do
     task.excecutedPop.should == "java -jar lib/jsTestDriver-1.2.1.jar --port 9876 "+
             "--browser iexplore.exe,firefox.exe --tests all"
 
-    expected = YAML::load(File.read("data/jstest/jsTestDriver_expected.conf"))
-    actual = YAML::load(File.read("data/output/jsTestDriver.conf"))
-
-    actual.should same_js_config(expected)    
+    "data/output/jsTestDriver.conf".should have_same_config_as "data/jstest/jsTestDriver_expected.conf"
   end
 
   it "Standard Test With Browsers and Files In CI tool" do
@@ -53,10 +61,7 @@ describe BW::JsTest do
     task.excecutedPop.should == "java -jar lib/jsTestDriver-1.2.1.jar --port 9876 "+
             "--browser iexplore.exe,firefox.exe --tests all --testOutput ."
 
-    expected = YAML::load(File.read("data/jstest/jsTestDriver_expected.conf"))
-    actual = YAML::load(File.read("data/output/jsTestDriver.conf"))    
-
-    actual.should same_js_config(expected)
+    "data/output/jsTestDriver.conf".should have_same_config_as "data/jstest/jsTestDriver_expected.conf"
   end
 
   it "Standard Test With Browsers and Files with manual XML config" do
@@ -70,10 +75,7 @@ describe BW::JsTest do
     task.excecutedPop.should == "java -jar lib/jsTestDriver-1.2.1.jar --port 9876 "+
             "--browser iexplore.exe,firefox.exe --tests all --testOutput ."
 
-    expected = YAML::load(File.read("data/jstest/jsTestDriver_expected.conf"))
-    actual = YAML::load(File.read("data/output/jsTestDriver.conf"))
-
-    actual.should same_js_config(expected)
+    "data/output/jsTestDriver.conf".should have_same_config_as "data/jstest/jsTestDriver_expected.conf"
   end
 
   it "Custom version, Test Output, JAR path, port, and server" do
@@ -90,10 +92,7 @@ describe BW::JsTest do
     task.excecutedPop.should == "java -jar newpath/jsTestDriver-1.2.1.jar "+
             "--tests all --testOutput testdir"
 
-    expected = YAML::load(File.read("data/jstest/jsTestDriver_expected_custom.conf"))
-    actual = YAML::load(File.read("data/output/jsTestDriver.conf"))
-
-    actual.should same_js_config(expected)
+    "data/output/jsTestDriver.conf".should have_same_config_as "data/jstest/jsTestDriver_expected_custom.conf"
   end
 
   it "Should clean up generated file if JS-Test-Driver fails" do
