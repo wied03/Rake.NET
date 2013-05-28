00f14235138a9ae1129669436ca71f8037960d36
Brady Wied <brady@wied.us>
2013-05-28 06:43:16 -0500

    Added new non YAML reference

--- lib/config.rb
+++ lib/config.rb
@@ -1,7 +1,7 @@
-require "yaml_config"
+require "build_config"
 
 module BradyW
-  # Using the bwyamlconfig GEM, does a singleton fetch of properties from the YAML config files
+  # Using the bwbuildconfig GEM, does a singleton fetch of properties from the YAML config files
   class Config
     private_class_method :new
 
@@ -9,7 +9,7 @@ module BradyW
 
     # Retrieve (using lazy instantation) our properties
     def Config.props
-      @@props = BW::YAMLConfig.new.props unless @@props
+      @@props = BradyW::BuildConfig.new.props unless @@props
       @@props
     end
   end
