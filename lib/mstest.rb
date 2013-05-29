404512ad68964c5162a419006e4ec86e34e4b1d7
Brady Wied <brady@wied.us>
2013-05-28 23:32:09 -0500

    Added NUnit runner

--- lib/mstest.rb
+++ lib/mstest.rb
@@ -2,34 +2,34 @@ require 'basetask'
 require 'windowspaths'
 
 module BradyW
-    # Runs MSTest tests using Visual Studio's MSTest runner
-	class MSTest < BaseTask
-		include BradyW::WindowsPaths
+  # Runs MSTest tests using Visual Studio's MSTest runner
+  class MSTest < BaseTask
+    include BradyW::WindowsPaths
 
-        # *Required* Files/test containers to test
-		attr_accessor :files
+    # *Required* Files/test containers to test
+    attr_accessor :files
 
-        # *Optional* Version of Visual Studio/MSTest to use, defaults to 10.0 
-        attr_accessor :version
+    # *Optional* Version of Visual Studio/MSTest to use, defaults to 10.0
+    attr_accessor :version
 
-        private
-        
-		def exectask
-			shell "\"#{path}MSTest.exe\"#{testcontainers}"
-		end
-		
-		def testcontainers
-			specifier = " /testcontainer:"
-			mainstr = files.join(specifier)
-			specifier+mainstr
-        end
+    private
 
-        def path
-           visual_studio version
-        end
-		
-		def version
-			@version || "10.0"
-		end
-	end
+    def exectask
+      shell "\"#{path}MSTest.exe\"#{testcontainers}"
+    end
+
+    def testcontainers
+      specifier = " /testcontainer:"
+      mainstr = files.join(specifier)
+      specifier+mainstr
+    end
+
+    def path
+      visual_studio version
+    end
+
+    def version
+      @version || "10.0"
+    end
+  end
 end
---
+++ spec/nunit_spec.rb
@@ -0,0 +1,41 @@
+require "base"
+require "nunit"
+require "basetaskmocking"
+
+describe BradyW::Nunit do
+
+  it 'shows correct default command line' do
+    task = BradyW::Nunit.new do |test|
+      test.files = ["file1.dll", "file2.dll"]
+    end
+    task.exectaskpublic
+    task.excecutedPop.should == "\"C:\\Program Files\\NUnit-2.6.2\\bin\\nunit-console.exe\" /framework=4.5 file1.dll file2.dll"
+  end
+
+  it 'uses NUnit 2.6.1' do
+      task = BradyW::Nunit.new do |test|
+        test.files = ["file1.dll", "file2.dll"]
+        test.version = "2.6.1"
+      end
+      task.exectaskpublic
+      task.excecutedPop.should == "\"C:\\Program Files\\NUnit-2.6.1\\bin\\nunit-console.exe\" /framework=4.5 file1.dll file2.dll"
+    end
+
+  it 'uses a configured custom path' do
+    task = BradyW::Nunit.new do |test|
+      test.files = ["file1.dll", "file2.dll"]
+      test.path = "C:\\SomeOtherplace"
+    end
+    task.exectaskpublic
+    task.excecutedPop.should == "\"C:\\SomeOtherplace\\nunit-console.exe\" /framework=4.5 file1.dll file2.dll"
+  end
+
+  it 'uses .NET 3.5' do
+      task = BradyW::Nunit.new do |test|
+        test.files = ["file1.dll", "file2.dll"]
+        test.framework_version = :v3_5
+      end
+      task.exectaskpublic
+      task.excecutedPop.should == "\"C:\\Program Files\\NUnit-2.6.2\\bin\\nunit-console.exe\" /framework=3.5 file1.dll file2.dll"
+    end
+end
