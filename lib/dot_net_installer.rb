55f9e530ef524dd0f6fd02b3b346d7fd14433403
Brady Wied <brady@wied.us>
2013-11-21 01:10:33 -0600

    Added empty test + get class ready

--- lib/dot_net_installer.rb
+++ lib/dot_net_installer.rb
@@ -1,9 +1,11 @@
 require 'basetask'
+require 'path_fetcher'
 
-class DotNetInstaller
+module BradyW
+  class DotNetInstaller < BaseTask
 
-  # TODO: Use the .net installer module as a default path for these tools
-  # TODO: Task 1: Paraffin executor (arrays for ext and the directory exclude)
-  # TODO: Task 3: WIX task (calls msbuild task and task #3 using config and /p:ProductVersion=1.0.0.0 /p:UpgradeCode=6c6bbe03-e405-4e6e-84ac-c5ef16f243e7)
-  # TODO: Task 2: DotNetinstaller task.  should do token replace in XML,make a temp copy, then call something like this: "%DNET_INSTALLER_PATH%\InstallerLinker.exe" /c:dotnetinstaller.xml /o:bin\Release\Bsw.Coworking.Agent.Installer.exe /t:"%DNET_INSTALLER_PATH%\dotNetInstaller.exe"
+    # TODO: Use the .net installer module as a default path for these tools
+    # TODO: Task 3: WIX task (calls msbuild task and task #3 using config and /p:ProductVersion=1.0.0.0 /p:UpgradeCode=6c6bbe03-e405-4e6e-84ac-c5ef16f243e7)
+    # TODO: Task 2: DotNetinstaller task.  should do token replace in XML,make a temp copy, then call something like this: "%DNET_INSTALLER_PATH%\InstallerLinker.exe" /c:dotnetinstaller.xml /o:bin\Release\Bsw.Coworking.Agent.Installer.exe /t:"%DNET_INSTALLER_PATH%\dotNetInstaller.exe"
+  end
 end
---
+++ spec/dot_net_installer_spec.rb
@@ -0,0 +1,15 @@
+require 'base'
+require 'dot_net_installer'
+require 'basetaskmocking'
+
+describe BradyW::DotNetInstaller do
+
+  it 'should do something' do
+    # arrange    
+
+    # act
+
+    # assert
+    true.should == false
+  end
+end
