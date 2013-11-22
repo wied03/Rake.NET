ad760f2b289c71082df1f45dd72973a6731d96b2
Brady Wied <brady@wied.us>
2013-11-21 18:02:00 -0600

    Add impl + test shell

---
+++ lib/wix_coordinator.rb
@@ -0,0 +1,8 @@
+require 'basetask'
+require 'msbuild'
+
+module BradyW
+  class WixCoordinator < BaseTask
+    # TODO: Task 3: WIX task (calls msbuild task and task #3 using config and /p:ProductVersion=1.0.0.0 /p:UpgradeCode=6c6bbe03-e405-4e6e-84ac-c5ef16f243e7)
+  end
+end
---
+++ spec/wix_coordinator_spec.rb
@@ -0,0 +1,23 @@
+require 'base'
+require 'wix_coordinator'
+require 'basetaskmocking'
+
+describe BradyW::WixCoordinator do
+  it 'should declare a Paraffin update, MSBuild, and DotNetInstaller task as dependencies' do
+    # arrange    
+
+    # act
+
+    # assert
+    true.should == false
+  end
+
+  it 'execute each dependency' do
+    # arrange
+
+    # act
+
+    # assert
+    fail 'Write this test'
+  end
+end
