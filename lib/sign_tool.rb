b5549aa391ce2fb40bc06b0077e0f1f8bd8bc629
Brady Wied <brady@wied.us>
2013-11-26 16:34:58 -0600

    Added test

---
+++ lib/sign_tool.rb
@@ -0,0 +1,6 @@
+require 'basetask'
+
+module BradyW
+  class SignTool < BaseTask
+  end
+end
---
+++ spec/sign_tool_spec.rb
@@ -0,0 +1,40 @@
+require 'base'
+require 'sign_tool'
+
+describe BradyW::SignTool do
+  it 'should execute properly with a certificate in the certificate store and defualt timestamp' do
+    # arrange
+    task = BradyW::SignTool.new do |t|
+      t.subject = 'The Subject'
+      t.description = 'The description'
+      t.sign_this = 'something.exe'
+    end
+    task.stub(:signtool_exe).with(:x64).and_return('path/to/x64/signtool.exe')
+
+    # act
+    task.exectaskpublic
+    command = task.executedPop
+
+    # assert
+    command.should == 'path/to/x64/signtool.exe sign /n "The Subject" /t http://timestamp.verisign.com/scripts/timestamp.dll /d "The description" something.exe'
+  end
+
+  it 'should work properly with a custom timestamp and custom architecture' do
+    # arrange
+    task = BradyW::SignTool.new do |t|
+      t.subject = 'The Subject'
+      t.description = 'The description'
+      t.sign_this = 'something.exe'
+      t.timestamp_url = 'http://something/timestamp.dll'
+      t.architecture = :x86
+    end
+    task.stub(:signtool_exe).with(:x86).and_return('path/to/x86/signtool.exe')
+
+    # act
+    task.exectaskpublic
+    command = task.executedPop
+
+    # assert
+    command.should == 'path/to/x86/signtool.exe sign /n "The Subject" /t http://something/timestamp.dll /d "The description" something.exe'
+  end
+end
