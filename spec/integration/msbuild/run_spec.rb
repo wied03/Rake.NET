03339ae43680da4af270e103739790bb2912d9be
Brady Wied <brady@bswtechconsulting.com>
2015-08-21 17:32:29 -0600

    Add basic spec in, fix Gemfile, simplify Rakefile

--- spec/integration/msbuild/run_spec.rb
+++ spec/integration/msbuild/run_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe BradyW::MSBuild do
-  pending 'write this'
+  RSpec::Matchers.define :run_successfully do
+    match do
+      @success
+    end
+
+    failure_message do
+      'Expected task to succeed but it did not'
+    end
+  end
+
+  describe 'build' do
+    before do
+      Dir.chdir(File.join(File.dirname(__FILE__), 'project')) do
+        Bundler.clean_system 'bundle exec rake build'
+        @success = $?.success?
+      end
+    end
+
+    it { is_expected.to run_successfully }
+    pending 'finish'
+  end
+
+  describe 'clean' do
+    pending 'write this'
+  end
 end
