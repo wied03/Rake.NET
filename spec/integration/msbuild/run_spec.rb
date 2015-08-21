56803e555147ed54264c0c4fdc443a90b48d8996
Brady Wied <brady@bswtechconsulting.com>
2015-08-21 17:56:45 -0600

    Add a more legit spec

--- spec/integration/msbuild/run_spec.rb
+++ spec/integration/msbuild/run_spec.rb
@@ -11,19 +11,54 @@ describe BradyW::MSBuild do
     end
   end
 
-  describe 'build' do
-    before do
-      Dir.chdir(File.join(File.dirname(__FILE__), 'RakeDotNet')) do
-        Bundler.clean_system 'bundle exec rake build'
-        @success = $?.success?
-      end
+  RSpec::Matchers.define :have_dll_files do |matcher|
+    def actual
+      Dir.glob(File.join(project_dir, '**/*.dll'))
     end
 
+    match do
+      matcher.matches? actual
+    end
+
+    failure_message do
+      matcher.failure_message
+    end
+  end
+
+  let(:project_dir) { File.join(File.dirname(__FILE__), 'RakeDotNet') }
+
+  before do
+    Dir.chdir(project_dir) do
+      Bundler.clean_system "bundle exec rake #{[*rake_targets].join ' '}"
+      @success = $?.success?
+    end
+  end
+
+  describe 'build' do
+    before do
+      Dir.glob(File.join(project_dir, '**/*.dll')).each { |f| File.delete f }
+    end
+
+    let(:rake_targets) { :build }
+
     it { is_expected.to run_successfully }
-    pending 'finish'
+    it { is_expected.to have_dll_files include /RakeDotNet.dll/ }
   end
 
   describe 'clean' do
-    pending 'write this'
+    let(:test_filename) { File.join(project_dir, 'RakeDotNet.dll') }
+
+    before do
+      FileUtils.touch test_filename
+    end
+
+    after do
+      FileUtils.rm_rf test_filename
+    end
+
+    let(:rake_targets) { :clean }
+
+    it { is_expected.to run_successfully }
+    it { is_expected.to_not have_dll_files include /RakeDotNet.dll/ }
   end
 end
