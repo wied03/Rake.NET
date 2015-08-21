c782c64d58180fae598f291855cbe55ea0dbf479
Brady Wied <brady@bswtechconsulting.com>
2015-08-21 17:39:33 -0600

    Create a new VS2015 project

--- spec/integration/msbuild/project/Database/schema/drop_database.sql
+++
@@ -1,8 +0,0 @@
-:On Error ignore
-ALTER DATABASE [$(dbname)] SET SINGLE_USER WITH ROLLBACK IMMEDIATE
-GO
-EXEC msdb.dbo.sp_delete_database_backuphistory @database_name = N'$(dbname)'
-GO
-DROP DATABASE [$(dbname)]
-GO
-:On Error exit
--- spec/integration/msbuild/project/Javascript/test/frontend/js/addertest.js
+++
@@ -1,7 +0,0 @@
-TestCase("adderTest", {
-	testAdd : function() {
-	  var adder = brady.firstrails.logic;
-	  assertEquals(3, adder.addstuff());
-	  assertEquals(6, adder.addstuff());
-	}
-});
