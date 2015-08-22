db52d2540ce7dfa880b88cdefda97281a518017d
Brady Wied <brady@bswtechconsulting.com>
2015-08-22 13:41:05 -0600

    Solution is optional

--- spec/integration/msbuild/RakeDotNet/rakefile.rb
+++ spec/integration/msbuild/RakeDotNet/rakefile.rb
@@ -1,11 +1,7 @@
 require 'rakedotnet'
 
-solution = 'RakeDotNet.sln'
 BradyW::MSBuild.new :clean do |clean|
   clean.targets = 'clean'
-  clean.solution = solution
 end
 
-BradyW::MSBuild.new :build do |build|
-  build.solution = solution
-end
+BradyW::MSBuild.new :build
