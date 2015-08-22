3b1a771e2e6dc9248e5139657e56a073b681bcd8
Brady Wied <brady@bswtechconsulting.com>
2015-08-22 12:36:09 -0600

    More cleanup

--- spec/unit/build/msbuild_spec.rb
+++ spec/unit/build/msbuild_spec.rb
@@ -132,73 +132,52 @@ describe BradyW::MSBuild do
 
   context 'properties' do
     context 'value includes spaces' do
-      pending 'write this'
+      let(:task_block) { lambda { |t| t.properties = {:prop1 => 'the value'} } }
+
+      it { is_expected.to execute_commands eq '"C:\\Program Files (x86)\\MSBuild\\14.0\\bin\\MSBuild.exe" /property:Configuration=Debug /property:TargetFrameworkVersion=v4.5 /property:prop1="the value"' }
     end
 
     context 'value includes semicolons' do
-      pending 'write this'
+      let(:task_block) { lambda { |t| t.properties = {:prop1 => 'the;value'} } }
+
+      it { is_expected.to execute_commands eq '"C:\\Program Files (x86)\\MSBuild\\14.0\\bin\\MSBuild.exe" /property:Configuration=Debug /property:TargetFrameworkVersion=v4.5 /property:prop1="the;value"' }
     end
 
     context 'custom properties that are also defaults' do
-      pending 'write this'
+      let(:task_block) do
+        lambda do |t|
+          t.properties = {
+              Configuration: 'myconfig',
+              prop2: 'prop2val'
+          }
+          t.build_config = :Release
+        end
+      end
+
+      it { is_expected.to execute_commands eq '"C:\\Program Files (x86)\\MSBuild\\14.0\\bin\\MSBuild.exe" /property:Configuration=myconfig /property:TargetFrameworkVersion=v4.5 /property:prop2=prop2val' }
     end
+  end
+
+  context 'explicit solution' do
+    pending 'write this'
   end
 
   context 'target' do
     pending 'write this'
   end
 
-  context 'full enchilada' do
-    pending 'write this'
-  end
+  context 'build config' do
+    context 'explicit debug' do
+      let(:task_block) { lambda { |t| t.build_config = :Debug } }
 
-  context 'explicit debug' do
-    pending 'write this'
-  end
-
-  xit 'should build OK vanilla (.NET 4.5)' do
-    # arrange
-    task = BradyW::MSBuild.new
-    expect(task).to receive(:dotnet).with("v4\\Client").and_return("C:\\yespath\\")
-
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
-
-    # assert
-    execed.should == 'C:\\yespath\\msbuild.exe /property:Configuration=Debug /property:TargetFrameworkVersion=v4.5'
-  end
-
-  xit 'should work OK with spaces in property value' do
-    # arrange
-    task = BradyW::MSBuild.new do |m|
-      m.properties = {:prop1 => 'the value'}
-      m.solution = 'stuff.sln'
+      it { is_expected.to execute_commands eq '"C:\\Program Files (x86)\\MSBuild\\14.0\\bin\\MSBuild.exe" /property:Configuration=Debug /property:TargetFrameworkVersion=v4.5' }
     end
-    expect(task).to receive(:dotnet).with("v4\\Client").and_return("C:\\yespath\\")
 
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
+    context 'release' do
+      let(:task_block) { lambda { |t| t.build_config = :Release } }
 
-    # assert
-    execed.should include '/property:Configuration=Debug /property:TargetFrameworkVersion=v4.5 /property:prop1="the value"'
-  end
-
-  xit 'should work OK with semicolons in the property value since MSBuild will otherwise interpret them as multiple property values' do
-    # arrange
-    task = BradyW::MSBuild.new do |m|
-      m.properties = {:prop1 => 'the;value'}
-      m.solution = 'stuff.sln'
+      it { is_expected.to execute_commands eq '"C:\\Program Files (x86)\\MSBuild\\14.0\\bin\\MSBuild.exe" /property:Configuration=Release /property:TargetFrameworkVersion=v4.5' }
     end
-    expect(task).to receive(:dotnet).with("v4\\Client").and_return("C:\\yespath\\")
-
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
-
-    # assert
-    execed.should include '/property:Configuration=Debug /property:TargetFrameworkVersion=v4.5 /property:prop1="the;value"'
   end
 
   xit 'should build OK (.NET 4.0)' do
@@ -246,52 +225,5 @@ describe BradyW::MSBuild do
 
     # assert
     execed.should == 'C:\\yespath2\\msbuild.exe /target:t1 /target:t2 /property:Configuration=Release /property:TargetFrameworkVersion=v3.5 /property:prop1=prop1val /property:prop2=prop2val solutionhere'
-  end
-
-  xit 'should build OK with custom properties that are also defaults (.NET 4.0)' do
-    # arrange
-    task = BradyW::MSBuild.new do |t|
-      t.properties = {:Configuration => 'myconfig',
-                      :prop2 => 'prop2val'}
-      t.build_config = :Release
-    end
-    expect(task).to receive(:dotnet).with("v4\\Client").and_return("C:\\yespath2\\")
-
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
-
-    # assert
-    execed.should == 'C:\\yespath2\\msbuild.exe /property:Configuration=myconfig /property:TargetFrameworkVersion=v4.5 /property:prop2=prop2val'
-  end
-
-  xit 'should work OK with build type set to Debug explicitly' do
-    # arrange
-    task = BradyW::MSBuild.new do |t|
-      t.build_config = :Debug
-    end
-    expect(task).to receive(:dotnet).with("v4\\Client").and_return("C:\\yespath2\\")
-
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
-
-    # assert
-    execed.should == 'C:\\yespath2\\msbuild.exe /property:Configuration=Debug /property:TargetFrameworkVersion=v4.5'
-  end
-
-  xit 'should build OK with .NET 2.0' do
-    # arrange
-    task = BradyW::MSBuild.new do |t|
-      t.dotnet_bin_version = :v2_0
-      t.compile_version = :v2_0
-    end
-
-    # act
-    task.exectaskpublic
-    execed = task.executedPop
-
-    # assert
-    execed.should == 'C:\\Windows\\Microsoft.NET\\Framework\\v2.0.50727\\msbuild.exe /property:Configuration=Debug /property:TargetFrameworkVersion=v2.0'
   end
 end
