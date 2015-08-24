0aa31ae2d853f45f04ecfc49d1bfe274fb894018
Brady Wied <brady@bswtechconsulting.com>
2015-08-24 12:35:29 -0600

    Add a compile dependency to ensure our restore works properly

--- spec/integration/msbuild/RakeDotNet/ClassLibrary1/Class1.cs
+++ spec/integration/msbuild/RakeDotNet/ClassLibrary1/Class1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace ClassLibrary1
 {
     public class Class1
     {
+        private void Test()
+        {
+            JsonConvert.SerializeObject("foobar");
+        }
     }
 }
