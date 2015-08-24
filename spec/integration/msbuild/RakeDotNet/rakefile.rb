99223f599b29957d14c6c6d5f1870820a15bb97a
Brady Wied <brady@bswtechconsulting.com>
2015-08-24 12:31:26 -0600

    Just use a pure Rake approach for this

--- spec/integration/msbuild/RakeDotNet/rakefile.rb
+++ spec/integration/msbuild/RakeDotNet/rakefile.rb
@@ -1,7 +1,12 @@
 require 'rakedotnet'
 
+desc 'Builds NuGet packages'
+file 'packages' => FileList['**/packages.config'] do
+	sh 'tools/nuget.exe restore RakeDotNet.sln'
+end
+
 BradyW::MSBuild.new :clean do |clean|
   clean.targets = 'clean'
 end
 
-BradyW::MSBuild.new :build
+BradyW::MSBuild.new :build => :packages
